b810b0cb4bc95130d7582e5a08f10fbe877d57c2
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2025-01-31 19:12:02 -0700

    added data to json file and fetching code

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Card = () => {
-    return <h2>movie #1</h2>;
-};
+//display information about the movies
+const Card = ({movie}) => {
+    return (
+        <div>
+            <h2>
+                {`#${-movie.ranking} - ${movie.title} (${movie.year})`}
+            </h2>
+            <img src={movie.img.src} alt={-movie.img.alt} width='200' />
+            <p>{`Distributor: ${movie.distributor}`}</p>
+            <p>{`Amount: ${movie.amount}`}</p>
+        </div>
+    );
+} ;
 
 export default Card;
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,4 +1,34 @@
 import React, { Component } from 'react';
 import Card from '../components/Card/Card';
 
-class List extends Component { r
+//retreive the data. 
+class List extends Component {
+    constructor(){
+    super()
+    this.state={
+        data:[],
+        loading:true,
+    };
+} 
+//fetch the data after the List component is mounted. replace empty array(from constructor) with the movie info
+async componentDidMount(){
+    const movies = await frtch('../..assets/data.json');
+    const moviesJSON = await movies.json();
+    if (moviesJSON){
+        this.setState({
+            data: moviesJSON,
+            loading:false,
+        });
+    }
+}
+//pass the state to the Card component
+        render() {
+    const {data, loading} = this.state;
+    if(loading){
+        return <div>Loading...</div>
+    }
+    return data.map(movie => <Card key={movie.id} movie={movie} />);
+}
+};
+
+export default List;
